a463ed6c13e4c4edb80afb3a9b7aad9b59b0876d
Dominik <example@gmail.com>
2026-06-26 13:50:49 +0300

    third commit

--- project2.py
+++ project2.py
@@ -4,7 +4,7 @@ salary = float(input('Введите почасовую ставку: '))
 hours_worked = float(input('Введите сколько часов вы отработали: '))
 tax_free = 550
 
-before_tax = salary * hour
+before_tax = salary * hours_worked
 after_tax1 = before_tax * 0.895
 after_tax2 = tax_free + (after_tax1 - tax_free) * 0.77
 
