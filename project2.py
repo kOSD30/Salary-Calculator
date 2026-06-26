1bff2fed0470f2c7d021af1c6aa88fee310caafe
Dominik <example@gmail.com>
2026-06-26 13:45:28 +0300

    second commit

--- project2.py
+++ project2.py
@@ -1,12 +1,12 @@
-print('=== Калькулятор зарплаты ===');
+print('=== Калькулятор зарплаты ===')
 
-salary = float(input('Введите почасовую ставку: '));
-hour = float(input('Введите сколько часов вы отработали: '));
+salary = float(input('Введите почасовую ставку: '))
+hours_worked = float(input('Введите сколько часов вы отработали: '))
 tax_free = 550
 
-before_tax = salary * hour;
-after_tax1 = before_tax * 0.895;
-after_tax2 = tax_free + (after_tax1 - tax_free) * 0.77;
+before_tax = salary * hour
+after_tax1 = before_tax * 0.895
+after_tax2 = tax_free + (after_tax1 - tax_free) * 0.77
 
 print(f'Зарплата до уплаты налогов: {before_tax:.2f}$')
 print(f'Зарплата после уплаты налогов: {after_tax2:.2f}$')
